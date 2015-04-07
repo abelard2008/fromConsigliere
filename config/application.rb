247955724bfdf5f4baa4c08daa6520cd4e0a72b8
Dillon Peng <pengcz.nwpu@gmail.com>
2015-04-07 10:38:08 +0800

    Generated scaffold article

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,10 @@ Bundler.require(*Rails.groups)
 
 module Consigliere
   class Application < Rails::Application
+
+    config.generators do |g|
+      g.test_framework nil, :fixture => false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
